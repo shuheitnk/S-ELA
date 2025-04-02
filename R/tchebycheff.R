2b5607880f9f748fe85a13e8bd0adcfb1572f362
Shuhei Tanaka <shuhei150228@gmail.com>
2025-04-03 04:46:23 +0900

    Delete R directory
    Signed-off-by: Shuhei Tanaka <shuhei150228@gmail.com>

--- R/tchebycheff.R
+++
@@ -1,7 +0,0 @@
-tchebycheff <- function(Y, w){
-  z <- apply(Y, 2, min)
-  G <- sapply(1:nrow(Y), function(j) {
-    max(w * (Y[j, ] - z))
-  })
-  return(G)
-}
--- R/weightedsum.R
+++
@@ -1,6 +0,0 @@
-weightedsum <- function(Y, w){
-  G <- sapply(1:nrow(Y), function(j) {
-    sum(w * Y[j, ])
-  })
-  return(G)
-}
