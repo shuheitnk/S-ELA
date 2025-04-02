3b2c0bbf24cb98e464d0df82df2bb589689196bd
Shuhei Tanaka <shuhei150228@gmail.com>
2025-04-03 00:19:12 +0900

    Update makeobject.R
    Signed-off-by: Shuhei Tanaka <shuhei150228@gmail.com>

--- makeobject.R
+++ makeobject.R
@@ -11,50 +11,37 @@ makeobject <- function(fn, n.sample) {
   fn.lower <- smoof::getLowerBoxConstraints(fn)
   fn.upper <- smoof::getUpperBoxConstraints(fn)
   
-  if (length(fn.lower) < 2 || length(fn.upper) < 2) {
-    stop("The function must have at least two dimensions for x1 and x2.")
+  d <- length(fn.lower)  # Number of decision variables
+  if (d < 1) {
+    stop("The function must have at least one decision variable.")
   }
   
-  # Set ranges for x1 and x2
-  x1_range <- c(fn.lower[1], fn.upper[1])  # Range for x1
-  x2_range <- c(fn.lower[2], fn.upper[2])  # Range for x2
-  
   # Generate initial samples
-  samples <- lhs::improvedLHS(n.sample, 2)
+  samples <- lhs::improvedLHS(n.sample, d)
   
   # Scale samples to the defined ranges
-  samples <- (samples * c(diff(x1_range), diff(x2_range))) + c(x1_range[1], x2_range[1])
+  samples <- sweep(samples, 2, fn.lower, FUN = "*") + fn.lower
   
-  # Apply the function to samples
-  y <- t(apply(samples, 1, fn))  # Transpose to ensure correct structure
+  # Evaluate the function
+  y <- t(apply(samples, 1, fn))
   
-  # Split y into y1 and y2
-  y1 <- y[, 1]
-  y2 <- y[, 2]
+  # Ensure y is a matrix
+  if (is.vector(y)) {
+    y <- matrix(y, ncol = 1)
+  }
   
-  # Calculate min and max for scaling
-  y1_min <- min(y1)
-  y1_max <- max(y1)
-  y2_min <- min(y2)
-  y2_max <- max(y2)
+  m <- ncol(y)  # Number of objectives
   
-  # Scale y1 and y2
-  y1 <- (y1 - y1_min) / (y1_max - y1_min)
-  y2 <- (y2 - y2_min) / (y2_max - y2_min)
-  
-  # Calculate objective scaling
-  obj_scale <- c((y1_max - y1_min), (y2_max - y2_min))
-  
-  # Update y with scaled values
-  y[, 1] <- y1
-  y[, 2] <- y2
+  # Scale objectives
+  y_min <- apply(y, 2, min)
+  y_max <- apply(y, 2, max)
+  obj_scale <- y_max - y_min
+  y <- sweep(y, 2, y_min, FUN = "-") / obj_scale
   
   # Scale samples to [0, 1]
-  samples[, 1] <- (samples[, 1] - x1_range[1]) / diff(x1_range)
-  samples[, 2] <- (samples[, 2] - x2_range[1]) / diff(x2_range)
+  samples <- sweep(samples, 2, fn.lower, FUN = "-") / (fn.upper - fn.lower)
   
-  # Return samples and y
+  # Return results
   return(list(samples = samples, y = y, obj_scale = obj_scale))
 }
-
 
